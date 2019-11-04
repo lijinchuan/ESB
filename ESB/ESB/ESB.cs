73e96f57dc8f771fbbfd797a345e1e6f4bff2b94
lijinchuan <403479851@qq.com>
2019-11-04 23:06:20 +0800

    一些修改

--- ESB/ESB/ESB.cs
+++ ESB/ESB/ESB.cs
@@ -25,48 +25,48 @@ namespace ESB
             esb.Error += new Action<Exception>(esb_Error);
             esb.StartServer();
 
-            LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
-            {
-                Info = "服务启动",
-                Level = LJC.Com.LogService.Contract.LogLevel.Info,
-                LogFrom = "ESB",
-                LogTime = DateTime.Now,
-                LogTitle = "esb服务启动",
-                LogType = LJC.Com.LogService.Contract.LogType.Service,
-                StackTrace = ""
-            });
+            //LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
+            //{
+            //    Info = "服务启动",
+            //    Level = LJC.Com.LogService.Contract.LogLevel.Info,
+            //    LogFrom = "ESB",
+            //    LogTime = DateTime.Now,
+            //    LogTitle = "esb服务启动",
+            //    LogType = LJC.Com.LogService.Contract.LogType.Service,
+            //    StackTrace = ""
+            //});
         }
 
         private void esb_Error(Exception ex)
         {
             LJC.FrameWork.LogManager.LogHelper.Instance.Error("esb报错", ex);
             //LJC.FrameWork.LogManager.Logger.TextLog("出错了", obj, LJC.FrameWork.LogManager.LogCategory.Other);
-            LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
-            {
-                Info=ex.Message,
-                Level=LJC.Com.LogService.Contract.LogLevel.Error,
-                LogFrom="ESB",
-                LogTime=DateTime.Now,
-                LogTitle="esb出错",
-                LogType=LJC.Com.LogService.Contract.LogType.Service,
-                StackTrace=ex.StackTrace
-            });
+            //LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
+            //{
+            //    Info=ex.Message,
+            //    Level=LJC.Com.LogService.Contract.LogLevel.Error,
+            //    LogFrom="ESB",
+            //    LogTime=DateTime.Now,
+            //    LogTitle="esb出错",
+            //    LogType=LJC.Com.LogService.Contract.LogType.Service,
+            //    StackTrace=ex.StackTrace
+            //});
         }
 
         protected override void OnStop()
         {
             // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
 
-            LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
-            {
-                Info = "服务停止",
-                Level = LJC.Com.LogService.Contract.LogLevel.Info,
-                LogFrom = "ESB",
-                LogTime = DateTime.Now,
-                LogTitle = "esb服务停止",
-                LogType = LJC.Com.LogService.Contract.LogType.Service,
-                StackTrace = ""
-            });
+            //LJC.Com.LogService.Contract.NetLogHelper.SendLog(new LJC.Com.LogService.Contract.LogInfo
+            //{
+            //    Info = "服务停止",
+            //    Level = LJC.Com.LogService.Contract.LogLevel.Info,
+            //    LogFrom = "ESB",
+            //    LogTime = DateTime.Now,
+            //    LogTitle = "esb服务停止",
+            //    LogType = LJC.Com.LogService.Contract.LogType.Service,
+            //    StackTrace = ""
+            //});
         }
     }
 }
