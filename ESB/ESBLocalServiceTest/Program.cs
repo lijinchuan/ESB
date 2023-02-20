97d26d8993d6881e81c5988a4df7ed3e2ff3ff54
ljc <403479851@qq.com>
2023-02-20 21:48:20 +0800

    支持WEB代理

---
+++ ESB/ESBLocalServiceTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBLocalServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LJC.FrameWork.SOA.ServiceConfig.WriteConfig(new List<LJC.FrameWork.SOA.WebMapper>
+            //{
+            //    new LJC.FrameWork.SOA.WebMapper
+            //    {
+            //        LocalHost="http://127.0.0.1:83/",
+            //        VirRoot="quancheng"
+            //    }
+            //}) ;
+
+            TestESBEervice service = new TestESBEervice();
+            service.LoginSuccess += new Action(() =>
+            {
+                service.RegisterService();
+                Console.WriteLine("注册成功");
+            });
+            service.Error += service_Error;
+            service.Login(null, null);
+
+            Console.ReadLine();
+
+            service.UnRegisterService();
+            service.Dispose();
+        }
+
+        static void service_Error(Exception obj)
+        {
+            Console.WriteLine(obj.Message);
+        }
+    }
+}
---
+++ ESB/ESBLocalServiceTest/TestESBEervice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBLocalServiceTest
+{
+    public class TestESBEervice : LJC.FrameWork.SOA.ESBService
+    {
+        public TestESBEervice()
+            : base(101, true, false)
+        {
+        }
+
+        public override object DoResponse(int funcId, byte[] Param, string clientid)
+        {
+            //var str = LJC.FrameWork.EntityBuf.EntityBufCore.DeSerialize<string>(Param);
+            //Console.WriteLine("收到消息:" + str);
+            //return funcId + ":" + str;
+
+            return base.DoResponse(funcId, Param, clientid);
+        }
+    }
+}
