6b46d2236d4fc00c369738351fa75a5ee781fb1f
ljc <403479851@qq.com>
2023-03-04 22:33:05 +0800

    网站代理服务

---
+++ ESB/Ljc.Com.WebProxyService/SvcHost.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljc.Com.WebProxyService
+{
+    public partial class SvcHost : ServiceBase
+    {
+        private WebProxySvc proxySvc = null;
+        private System.Timers.Timer timer = null;
+        public SvcHost()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            proxySvc = new WebProxySvc();
+            proxySvc.StartService();
+
+            timer = LJC.FrameWork.Comm.TaskHelper.SetInterval(1000 * 180, () =>
+            {
+                try
+                {
+                    var config = LJC.FrameWork.SOA.ServiceConfig.ReadConfig();
+                    foreach (var item in config.WebMappers)
+                    {
+                        try
+                        {
+                            var client = LJC.FrameWork.Comm.HttpClientFactory.GetHttpClient(item.TragetWebHost);
+                            _ = client.GetAsync(item.TragetWebHost).Result;
+
+                            LJC.FrameWork.LogManager.LogHelper.Instance.Debug("激活网站成功:"+item.TragetWebHost);
+                        }
+                        catch (Exception ex)
+                        {
+                            ex.Data.Add("url", item.TragetWebHost);
+                            LJC.FrameWork.LogManager.LogHelper.Instance.Error("激活网站失败", ex);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+                return false;
+            }, runintime: false);
+        }
+
+        protected override void OnStop()
+        {
+            if (proxySvc != null)
+            {
+                proxySvc.UnRegisterService();
+                proxySvc.Dispose();
+            }
+
+            if (timer != null)
+            {
+                timer.Dispose();
+            }
+        }
+    }
+}
---
+++ ESB/Ljc.Com.WebProxyService/WebProxySvc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ljc.Com.WebProxyService
+{
+    internal class WebProxySvc : LJC.FrameWork.SOA.ESBService
+    {
+        public WebProxySvc()
+            : base(101, false, false, "webProxy", System.Net.Dns.GetHostName())
+        {
+        }
+
+        protected override void OnError(Exception e)
+        {
+            LJC.FrameWork.LogManager.LogHelper.Instance.Error("服务出错", e);
+        }
+
+        public override object DoResponse(int funcId, byte[] Param, string clientid)
+        {
+            //var str = LJC.FrameWork.EntityBuf.EntityBufCore.DeSerialize<string>(Param);
+            //Console.WriteLine("收到消息:" + str);
+            //return funcId + ":" + str;
+
+            return base.DoResponse(funcId, Param, clientid);
+        }
+    }
+}
