e4098acaf075ce70cd24eee99b99cf2114dca30f
ljc <403479851@qq.com>
2023-03-11 16:03:01 +0800

    使用代理

--- ESB/ESBLocalServiceTest/Program.cs
+++ ESB/ESBLocalServiceTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LJC.FrameWork.SOA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,18 +14,30 @@ namespace ESBLocalServiceTest
     {
         static void Main(string[] args)
         {
-            //LJC.FrameWork.SOA.ServiceConfig.WriteConfig(new List<LJC.FrameWork.SOA.WebMapper>
+            //LJC.FrameWork.SOA.ServiceConfig.WriteConfig(new ServiceConfig(),
+            //    new List<LJC.FrameWork.SOA.WebMapper>
             //{
             //    new LJC.FrameWork.SOA.WebMapper
             //    {
             //        MappingPort=8082,
             //        MappingRoot=string.Empty,
-            //        TragetWebHost="http://127.0.0.1:83"
+            //        TragetWebHost="http://127.0.0.1:83",
+            //        UseProxyName="7890"
+            //    }
+            //},new List<LJC.FrameWork.SOA.WebProxy>
+            //{
+            //    new LJC.FrameWork.SOA.WebProxy
+            //    {
+            //        Address="127.0.0.1:7890",
+            //        Name="7890",
+            //        UserName="",
+            //        UserPassWord=""
             //    }
             //});
+
             //ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
             //System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12| SecurityProtocolType.Ssl3;
-            
+
             TestESBEervice service = new TestESBEervice();
             service.LoginSuccess += new Action(() =>
             {
